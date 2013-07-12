6b21d7827c0dd62abe7fb78a3ec720920944e208
Randall A. Gordon <randall@randallagordon.com>
2013-07-12 04:10:00 +0000

    Read index.html from file for request

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( fs.readFileSync( 'index.html', 'utf-8' ) );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
